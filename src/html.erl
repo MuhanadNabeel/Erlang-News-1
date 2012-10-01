161180185e8aac6662b74324dc4da16ad28f7696
MuhanadNabeel <Muhanad_88@hotmail.se>
2012-10-01 14:20:43 +0200

    commiting the structre for html process for the first time

--- src/html.erl
+++ src/html.erl
@@ -1,1 +1,29 @@
 -module(html).
+-compile(export_all).
+-record(state, {url="", ref="", resource="", ts="", tags=""}).
+
+%% Writting the sources required for this module
+start(URL, Ref, Source, Ts, Tags) ->
+%% Sending sources to the init() 
+    spawn(?MODULE, init, [URL, Ref, Source, Ts, Tags]).
+   
+%% Init assining the sources to the records received by the loop function
+init(URL, Ref, Source, Ts, Tags) ->
+    loop(#state{url=URL, ref=Ref, resource=Resource, ts=Ts, tags=Tags}).
+
+
+
+%% loop receives the sources and messages 
+loop(#state{}) ->
+    receive
+	_ ->
+	    ok
+    after T ->
+	    ok
+    end.
+
+	
+    		       
+
+    
+    
