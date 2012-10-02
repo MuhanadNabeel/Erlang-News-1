0ee73a48438bd63d7236eb198650a6d97c17eaf5
Ingimar <ingimar.samuelsson@gmail.com>
2012-10-02 15:32:50 +0200

    Changes done

--- src/parse_rss.erl
+++ src/parse_rss.erl
@@ -9,40 +9,79 @@
 read() ->
 	inets:start(),
 	{ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} =
-		  httpc:request("http://coder.io/tag/erlang.rss"),
+%		httpc:request("http://www.planeterlang.org/en/planet/rss_2.0"),
+		httpc:request("http://coder.io/tag/erlang.rss"),
+%		httpc:request("http://www.reddit.com/r/erlang.rss"),
 	Body.
 
 	
-devide(coder,List,TimeStamp) ->
+devide(Atom,List,TimeStamp) ->
 	[_|T] = parse(List),
-	iterate(T,TimeStamp,[]).
+	iterate(Atom,T,TimeStamp,[]).
 	
-iterate([],_TimeStamp,List) ->
+iterate(_Atom,[],_TimeStamp,List) ->
 	List;
 	
-iterate([H|T],TimeStamp,List) ->
-	Keys = proplists:get_value("keywords", H),
-	Index = string:rstr(Keys, "erlang"),
+iterate(Atom,[H|T],TimeStamp,List) ->
+	%Keys = proplists:get_value("keywords", H),
+	%Index = string:rstr(Keys, "erlang"),
 	PubDate = proplists:get_value("pubDate",H),	
-	Compare = compare_dates(TimeStamp,PubDate),
+	Compare = compare_dates(Atom,TimeStamp,PubDate),
 	if 
-		Index > 0, Compare >= 0 ->
+		Compare >= 0 -> % Index > 0, 
 			URL = proplists:get_value("link",H),
-			Tags = proplists:get_value("keywords",H),
-			iterate(T,TimeStamp,[#rss_item{link=URL,
-										pubDate=convert_pubDate_to_datetime(PubDate),
-										tags=Tags}|List]);
+			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+			%   % If using keywords                                     %
+			%Tags = list_tags(Atom,proplists:get_value("keywords",H)),  %
+			%iterate(Atom,T,TimeStamp,[#rss_item{link=URL, 	            %
+			%	pubDate=convert_pubDate_to_datetime(PubDate),           %
+			%	tags=Tags}|List]);							            %		
+			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+			iterate(Atom,T,TimeStamp,[#rss_item{link=URL,
+				pubDate=convert_pubDate_to_datetime(Atom,PubDate)}|List]);
 		true ->
-			iterate(T,TimeStamp,List)
+			iterate(Atom,T,TimeStamp,List)
 	end.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+% Depricated - keywords will be genereated %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%list_tags(coder,[[]|T],List) ->
+%	list_tags(coder,T,List);
+%	
+%list_tags(coder,["]"|T],List) ->
+%	list_tags(coder,T,List);
+%	
+%list_tags(coder,[H|T],List) ->
+%	list_tags(coder,T,[H|List]);
+%	
+%list_tags(coder,[],List) ->
+%	List;
+%	
+%list_tags(coder,String) ->
+%	[_|T] = re:split(String,"[\",\"]",[{return,list}]),
+%	list_tags(coder,T,[]).
 
-compare_dates(Date1,Date2) ->
-	calendar:datetime_to_gregorian_seconds(convert_pubDate_to_datetime(Date2))
+compare_dates(Atom,Date1,Date2) ->
+	calendar:datetime_to_gregorian_seconds(convert_pubDate_to_datetime(Atom,Date2))
 	-
-	calendar:datetime_to_gregorian_seconds(convert_pubDate_to_datetime(Date1)).
+	calendar:datetime_to_gregorian_seconds(Date1).
+	
+%convert_pubDate_to_datetime([]) ->
+%	[];
+	
+%convert_pubDate_to_datetime([H|T]) ->
+%	[string:to_integer(H)] ++ convert_pubDate_to_datetime(T);
 
+%convert_pubDate_to_datetime(planeterlang,DateTime) ->
+%	[H|T] = re:split(DateTime,"[T]",[{return,list}]),
+%	Date = re:split(H,"[-]",[{return,list}]),
+%	[F|B] = re:split(hd(T),"[+]",[{return,list}]),
+%	Time = re:split(F,"[:]",[{return,list}]),
+%	%TimeTuple = list_to_tuple(Time),
+%	convert_pubDate_to_datetime(Date ++ Time);
+		
 % Author:  Khashayar
-convert_pubDate_to_datetime(DateTime) ->
+convert_pubDate_to_datetime(_,DateTime) ->
     {ok,[_Day, Date, MonthS, Year,HH,MM,SS], _} = 
 	io_lib:fread("~3s, ~2d ~3s ~4d ~2d:~2d:~2d" , DateTime),
     Month = case MonthS of
