1cf6f3ff513558dc48036fe78bb060bcda592e1b
Ingimar <ingimar.samuelsson@gmail.com>
2012-10-02 20:54:39 +0200

    RSS record added to records.hrl

--- include/records.hrl
+++ include/records.hrl
@@ -1,4 +1,5 @@
 -record(checker_state,{source, time_stamp , next_check, items}).
 -record(sup_state,{processes,sources}).
 -record(checker,{pid,source_name,source}).
-
+%-record(rss_item,{link,pubDate,tags}). % If using keywords
+-record(rss_item,{link,pubDate}).
