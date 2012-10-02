e7079d21cfd3a0ec51b0a4150f07a41d80f4abc2
MuhanadNabeel <Muhanad_88@hotmail.se>
2012-10-02 09:44:05 +0200

    errors are gone!

--- src/html.erl
+++ src/html.erl
@@ -3,56 +3,58 @@
 -record(state, {url="", ref="", resource="", ts="", tags=""}).
 
 %% Writting the sources required for this module
-start(URL, Ref, Source, Ts, Tags) ->
+start(_URL, _Ref, _Source, _Ts, _Tags) ->
 %% Sending sources to the init() 
-    spawn(?MODULE, init, [URL, Ref, Source, Ts, Tags]).
+    spawn(?MODULE, init, [_URL, _Ref, _Source, _Ts, _Tags]).
    
 %% Init assining the sources to the records received by the loop function
-init(URL, Ref, Source, Ts, Tags) ->
-    loop(#state{url=URL, ref=Ref, resource=Resource, ts=Ts, tags=Tags}).
+init(_URL, _Ref, _Source, _Ts, _Tags) ->
+   %% loop(#state{url=URL, ref=Ref, resource=Resource, ts=Ts, tags=Tags}) ->
+					      ok.
 
 
 
 %% loop receives the sources and messages 
 loop(#state{}) ->
-    receive
-	_ ->
-	    ok
-    after 0 ->
+   ok.
+   %% receive
+   %%	_ ->
+	   
+   %%  after 0 ->
 	    %end url must be here!
 	    %checking if the URL exists
-	    check_duplicate(_message) ->
-		    ok.
+	    %%check_duplicate(_message) ->
+		    
             %% divide to html tags
             %% get meta data
             %% write to db
 
-           write_to_db(URL, Description, Title, Image, Icon) ->
-           %% send message to the CHECKER!
+            %%write_to_db(_URL, _Description, _Title, _Image, _Icon) ->
+            %% send message to the CHECKER!
 
-	    ok
-    end.
+	    
+   %% end.
 
 
 %% Find the end url
 
-end_url(URL, Description, Title, Image, Icon) ->
+end_url(_URL, _Description, _Title, _Image, _Icon) ->
     ok.
 
 
 %% checking if the URL exists in the database
-check_duplicate(News, URL) ->
-    
-    case db:exist{News, URL} of
-         true -> 'blabla';
+check_duplicate(_News, _URL) ->
+       ok.  
+   %% case db:exist{_News, _URL} of
+   %%      true -> 'blabla';
 	    %% do something
        
-         false -> 'bla'
+   %%       false -> 'bla'
 	    %% return error
-    end.
+   %%  end.
 %% fetching the html source 
 
-fetch_html_source(URL, Description, Title, Image, Icon) ->
+fetch_html_source(_URL, _Description, _Title, _Image, _Icon) ->
     ok.
 
 %% Dividing the URL into tags
@@ -69,8 +71,9 @@ get_meta_data(_Data) ->
 
 
 %% passing the sources to the database  
-write_to_db(URL, Description, Title, Image, Icon) ->
-    db:write(news, {URL, Description, Title, Image, Icon}) ->
-	ok.					     
+write_to_db(_URL, _Description, _Title, _Image, _Icon) ->
+    ok.
+ %% db:write(news, {_URL, _Description, _Title, _Image, _Icon}) ->
+						     
     
     
