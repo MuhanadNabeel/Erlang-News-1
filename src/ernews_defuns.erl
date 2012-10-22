0fff9b8706ab341563c8c5fc40576984b837d403
ingimars <ingimar.samuelsson@gmail.com>
2012-10-22 12:59:10 +0200

    defuns with error handling for httpc request

--- src/ernews_defuns.erl
+++ src/ernews_defuns.erl
@@ -1,12 +1,23 @@
 -module(ernews_defuns).
 -compile(export_all).
+
+read_web({ok, {{_Version, 200, _ReasonPhrase}, Headers, Body}}) ->
+	{Headers,Body}.
+	
+read_web(_) ->
+	{error,reason}.
+
+read_web(check,Src) ->
+	inets:start(),
+	read_web(httpc:request(Src)).
+
 	
 compare_dates(Date1,Date2) ->
 	calendar:datetime_to_gregorian_seconds(convert_pubDate_to_datetime(Date2))
 	-
 	calendar:datetime_to_gregorian_seconds(Date1).
 	
-% Author:  Khashayar
+% Author:  Khashayar Two
 convert_pubDate_to_datetime(DateTime) ->
     {ok,[_Day, Date, MonthS, Year,HH,MM,SS], _} = 
 	io_lib:fread("~3s, ~2d ~3s ~4d ~2d:~2d:~2d" , DateTime),
