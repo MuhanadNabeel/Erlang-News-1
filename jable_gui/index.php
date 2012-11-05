1f4cb17b4e293aa3fb80c285bc435bd83730808c
ingimars <ingimar.samuelsson@gmail.com>
2012-11-05 10:58:08 +0100

    index

--- jable_gui/index.php
+++ jable_gui/index.php
@@ -5,8 +5,11 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript" src="default_js.js"></script>
         <title></title>
     </head>
     <body>
+        <div id="test"></div>
     </body>
 </html>
