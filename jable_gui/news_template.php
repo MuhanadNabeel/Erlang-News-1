927511f8e9fee3b54c9591ae95cfae78334ce4d6
joelhjalta <joelhjalta@gmail.com>
2012-11-06 11:10:00 +0100

    blaberson

---
+++ jable_gui/news_template.php
@@ -0,0 +1,22 @@
+
+<div class="newsTemp"> 
+   <p>
+       <h1>$title</h1>
+   </p>
+
+   <p class="main">
+       <img src="$image" align="left">
+       $description
+   </p>  
+</div>
+<div style="position: relative;">
+    <div class="absolute_24x24_centerback" 
+         style="background-image: url('http://cdn3.iconfinder.com/data/icons/freeapplication/png/24x24/Thumbs%20up.png');left:10px; "></div>
+    <div class="absolute_24x24_centerback" 
+         style="background-image: url('http://cdn3.iconfinder.com/data/icons/freeapplication/png/24x24/Thumbs%20down.png');left:50px; "></div>
+    <div class="absolute_24x24_centerback" 
+         style="background-image: url('http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/24/Actions-edit-undo-icon.png');left:30px;"></div>
+    <div class="absolute_24x24_centerback" 
+         style="background-image: url('http://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/Warning.png');right:10px;"></div>
+</div>
+ 
