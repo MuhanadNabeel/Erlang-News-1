2db85c9d2099aba5587ae2d08cf10ded42c6c5ba
ingimars <ingimar.samuelsson@gmail.com>
2012-11-06 13:18:07 +0100

    button function started

--- jable_gui/news_template.php
+++ jable_gui/news_template.php
@@ -12,15 +12,15 @@
 <div style="position: relative; height: 30px; padding-bottom: 30px;">
     <div class="absolute_24x24_centerback" 
          style="background-image: url('http://cdn3.iconfinder.com/data/icons/freeapplication/png/24x24/Thumbs%20up.png');left:10px;"
-         id="{id}_vote_up" onclick="articleAction(1)"></div>
+         id="{id}_vote_up" onclick="articleAction(this,1,false)"></div>
     <div class="absolute_24x24_centerback" 
          style="background-image: url('http://cdn3.iconfinder.com/data/icons/freeapplication/png/24x24/Thumbs%20down.png');left:50px;"
-         id="{id}_vote_down" onclick="articleAction(0)"></div>
+         id="{id}_vote_down" onclick="articleAction(this,0,false)"></div>
     <div class="absolute_24x24_centerback" 
          style="background-image: url('http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/24/Actions-edit-undo-icon.png');left:30px;display:none;" 
-         id="{id}_vote_undo" onclick="undoArticleAction(0)"></div>
+         id="{id}_vote_undo" onclick="articleAction(this,0,true)"></div>
     <div class="absolute_24x24_centerback" 
          style="background-image: url('http://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/Warning.png');right:10px;"
-         id="{id}_report" onclick="articleAction()"></div>
+         id="{id}_report" onclick="articleAction(this,2,false)"></div>
 </div>
  
