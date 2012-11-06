bcb3c0aa3ed6f910a94d2f837e33b83a258369f7
joelhjalta <joelhjalta@gmail.com>
2012-11-06 14:40:36 +0100

    yup

--- jable_gui/news_template.php
+++ jable_gui/news_template.php
@@ -11,16 +11,16 @@
 </div>
 <div style="position: relative; height: 30px; padding-bottom: 30px;">
     <div class="absolute_24x24_centerback" 
-         style="background-image: url('http://cdn3.iconfinder.com/data/icons/freeapplication/png/24x24/Thumbs%20up.png');left:10px;"
+         style="background-image: url('http://icons.iconarchive.com/icons/fatcow/farm-fresh/16/thumb-up-icon.png');left:10px;"
          id="{id}_vote_up" onclick="articleAction(this,1,false)"></div>
     <div class="absolute_24x24_centerback" 
-         style="background-image: url('http://cdn3.iconfinder.com/data/icons/freeapplication/png/24x24/Thumbs%20down.png');left:50px;"
+         style="background-image: url('http://icons.iconarchive.com/icons/fatcow/farm-fresh/16/thumb-down-icon.png');left:40px;"
          id="{id}_vote_down" onclick="articleAction(this,0,false)"></div>
     <div class="absolute_24x24_centerback" 
          style="background-image: url('http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/24/Actions-edit-undo-icon.png');left:30px;display:none;" 
          id="{id}_vote_undo" onclick="articleAction(this,0,true)"></div>
     <div class="absolute_24x24_centerback" 
-         style="background-image: url('http://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/Warning.png');right:10px;"
+         style="background-image: url('http://icons.iconarchive.com/icons/kyo-tux/aeon/16/Sign-Alert-icon.png');right:10px;"
          id="{id}_report" onclick="articleAction(this,2,false)"></div>
 </div>
  
--- jable_gui/rightnews_template.php
+++ jable_gui/rightnews_template.php
@@ -1,4 +1,17 @@
-<div>
-    <a href="$URL">$title</a>
-    <div></div>
+<div class="right_recent">
+    <a href="">{title}</a>
+    <div style="position: relative; height: 30px;">
+    <div class="absolute_16x16_centerback" 
+         style="background-image: url('http://icons.iconarchive.com/icons/fatcow/farm-fresh/16/thumb-up-icon.png');left:10px;"
+         id="{id}_vote_up" onclick="articleAction(this,1,false)"></div>
+    <div class="absolute_16x16_centerback" 
+         style="background-image: url('http://icons.iconarchive.com/icons/fatcow/farm-fresh/16/thumb-down-icon.png');left:50px;"
+         id="{id}_vote_down" onclick="articleAction(this,0,false)"></div>
+    <div class="absolute_16x16_centerback" 
+         style="background-image: url('http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/24/Actions-edit-undo-icon.png');left:30px;display:none;" 
+         id="{id}_vote_undo" onclick="articleAction(this,0,true)"></div>
+    <div class="absolute_16x16_centerback" 
+         style="background-image: url('http://icons.iconarchive.com/icons/kyo-tux/aeon/16/Sign-Alert-icon.png');left:90px;"
+         id="{id}_report" onclick="articleAction(this,2,false)"></div>
+</div>
 </div>
