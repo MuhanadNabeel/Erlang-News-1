e90603106c9d179531d348292347b2b565f1b69a
joelhjalta <joelhjalta@gmail.com>
2012-11-06 18:43:21 +0100

    shizen

--- jable_gui/news_template.php
+++ jable_gui/news_template.php
@@ -3,7 +3,7 @@
        <a href="redirect.php?id={id}&url={URL}" onmouseover="JavaScript:windows.status='this link blaber';
            return true;" onmouseout="window.status=''";><h1>{title}</h1></a>
    </p>
-   <p class="main">
+   <p class="main" style="min-height: 70px;">
        <img src="{image}" align="left">
        {description}
    </p>  
@@ -16,13 +16,18 @@
          style="background-image: url('http://icons.iconarchive.com/icons/fatcow/farm-fresh/16/thumb-down-icon.png');left:40px;"
          id="{id}_vote_down" onclick="articleAction(this,0,false)"></div>
     <div class="absolute_24x24_centerback" 
-         style="background-image: url('http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/24/Actions-edit-undo-icon.png');left:30px;display:none;" 
-         id="{id}_vote_undo" onclick="articleAction(this,0,true)"></div>
+         style="background-color: black;left:10px;display:none;" 
+         id="{id}_vote_up_active" onclick="articleAction(this,0,true)"></div>
+    <div class="absolute_24x24_centerback" 
+         style="background-color: blue;left:10px;display:none       ;" 
+         id="{id}_vote_down_active" onclick="articleAction(this,0,true)"></div>     
     <div class="absolute_24x24_centerback" 
          style="background-image: url('http://icons.iconarchive.com/icons/kyo-tux/aeon/16/Sign-Alert-icon.png');right:10px;"
          id="{id}_report" onclick="articleAction(this,2,false)"></div>
-    <!--<span class="up_down_span" style="left: 50px;">
-        up/down
-    </span>-->     
+    <div class="up_down_div">
+        <span class="up_down_span" >
+            up/down
+        </span>
+    </div>
 </div>
  
--- jable_gui/rightnews_template.php
+++ jable_gui/rightnews_template.php
@@ -8,10 +8,13 @@
          style="background-image: url('http://icons.iconarchive.com/icons/fatcow/farm-fresh/16/thumb-down-icon.png');left:50px;"
          id="{id}_vote_down" onclick="articleAction(this,0,false)"></div>
     <div class="absolute_16x16_centerback" 
-         style="background-image: url('http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/24/Actions-edit-undo-icon.png');left:30px;display:none;" 
-         id="{id}_vote_undo" onclick="articleAction(this,0,true)"></div>
+         style="background-color:black;left:10px;display:none;" 
+         id="{id}_vote_up_active" onclick="articleAction(this,0,true)"></div>
+         <div class="absolute_16x16_centerback" 
+         style="background-color:blue;left:50px;display:none;" 
+         id="{id}_vote_down_active" onclick="articleAction(this,0,true)"></div>
     <div class="absolute_16x16_centerback" 
          style="background-image: url('http://icons.iconarchive.com/icons/kyo-tux/aeon/16/Sign-Alert-icon.png');left:90px;"
          id="{id}_report" onclick="articleAction(this,2,false)"></div>
-</div>
+    </div> 
 </div>
