a407142a662aad6043fda8b5104c41c1b23b52b9
ingimars <ingimar.samuelsson@gmail.com>
2012-11-11 11:56:29 +0100

    Sort by up_vote

--- jable_gui/_get_news.php
+++ jable_gui/_get_news.php
@@ -2,10 +2,11 @@
 if(class_exists('MySQL') === FALSE)
     include 'MySQL.php';
 $sql = new MySQL();
-$result = $sql->sqlQuery("SELECT * FROM ernews_news LIMIT 30");
+$result = $sql->sqlQuery("SELECT * FROM ernews_news ORDER BY Up_Vote DESC");
 $outcome = Array();
 while( ($row = mysql_fetch_array($result)) !== FALSE ) {
-    $row['host'] = parse_url($row['URL'])['host'];
+    $purl = parse_url($row['URL']);
+    $row['host'] = $purl['host'];
     $outcome[] = $row;
 }
 
