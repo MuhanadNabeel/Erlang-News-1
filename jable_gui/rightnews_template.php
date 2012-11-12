4879919ff92669ef020b9c849394e95210acb837
Pletron <philip.masek@gmail.com>
2012-11-12 16:29:44 +0100

    Revert "Fixing buttons"
    This reverts commit 72fcb3eca25492f3e1010d5b73cf9b3f1e42fa89.

--- jable_gui/rightnews_template.php
+++ jable_gui/rightnews_template.php
@@ -1,4 +1,4 @@
-<li data-id="{id}_right_item" data-type="right_article_{datatype}">
+<li data-id="{id}_right_item" data-type="right_{datatype}">
     <div>
 
     <div class="right_row" onclick="openUpStuff('{id}')">
@@ -18,10 +18,10 @@
 
                     <td class="voting">
                         <div>
-                            <div id="{id}_vote_up_{datatype}" class="thumb-up" onclick="articleAction(this,1,false)"></div>
-                            <div id="{id}_vote_up_{datatype}_active" class="thumb-up-active" onclick="articleAction(this,1,true)"></div>
-                            <div id="{id}_vote_down_{datatype}" class="thumb-down" onclick="articleAction(this,0,false)"></div>
-                            <div id="{id}_vote_down_{datatype}_active" class="thumb-down-active" onclick="articleAction(this,0,true)"></div>
+                            <div id="{id}_vote_up" class="thumb-up" onclick="articleAction(this,1,false)"></div>
+                            <div id="{id}_vote_up_active" class="thumb-up-active" onclick="articleAction(this,1,true)"></div>
+                            <div id="{id}_vote_down" class="thumb-down" onclick="articleAction(this,0,false)"></div>
+                            <div id="{id}_vote_down_active" class="thumb-down-active" onclick="articleAction(this,0,true)"></div>
                         </div>
                     </td>
                     <td>
