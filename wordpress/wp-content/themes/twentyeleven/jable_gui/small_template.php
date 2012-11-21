a7af73115d9ce711196e3f9e96a6ae1f38c7ce2a
Pletron <philip.masek@gmail.com>
2012-11-21 19:05:59 +0100

    stuff

--- wordpress/wp-content/themes/twentyeleven/jable_gui/small_template.php
+++ wordpress/wp-content/themes/twentyeleven/jable_gui/small_template.php
@@ -32,12 +32,12 @@
                         	<table id="{id}_all_vote_buttons" style="display:none;">
 	                        	<tr>
 		                    		<td>
-			                            <div id="{id}_vote_up_{datatype}" class="thumb-up-shadowed" onclick="articleAction(this,1,false);getNewsJSON(false, true)"></div>
-			                            <div id="{id}_vote_up_{datatype}_active" class="thumb-up-active-shadowed" onclick="articleAction(this,1,true);getNewsJSON(false, true)"></div>
+			                            <div id="{id}_vote_up_{datatype}_extra" class="thumb-up-shadowed" onclick="articleAction(this,1,false);getNewsJSON(false, true)"></div>
+			                            <div id="{id}_vote_up_{datatype}_extra_active" class="thumb-up-active-shadowed" onclick="articleAction(this,1,true);getNewsJSON(false, true)"></div>
 			                        </td>
 			                        <td>
-			                            <div id="{id}_vote_down_{datatype}" class="thumb-down-shadowed" onclick="articleAction(this,0,false);getNewsJSON(false, true)"></div>
-			                            <div id="{id}_vote_down_{datatype}_active" class="thumb-down-active-shadowed" onclick="articleAction(this,0,true);getNewsJSON(false, true)"></div>
+			                            <div id="{id}_vote_down_{datatype}_extra" class="thumb-down-shadowed" onclick="articleAction(this,0,false);getNewsJSON(false, true)"></div>
+			                            <div id="{id}_vote_down_{datatype}_extra_active" class="thumb-down-active-shadowed" onclick="articleAction(this,0,true);getNewsJSON(false, true)"></div>
 		                            </td>
 	                        	</tr>
                         	</table>
