57c225a4eff6ceff9b03716e8e281b4dba1c1d51
MuhanadNabeel <Muhanad_88@hotmail.se>
2012-12-13 18:00:01 +0100

    added comments to the ernews_app

--- src/ernews_app.erl
+++ src/ernews_app.erl
@@ -1,3 +1,13 @@
+%%% @author Muhanad Nabeel <Muhanad@dhcp-207005.eduroam.chalmers.se>
+%%% @copyright (C) 2012, Muhanad Nabeel
+%%% @doc
+%%%     This module is the application module starting the supervisor
+%%%     including the error loggers to generate report file
+%%%     And also stops it
+%%% @end
+%%% Created : 13 Dec 2012 by Muhanad Nabeel <Muhanad@dhcp-207005.eduroam.chalmers.se>
+
+
 %%% API module for the pool
 -module(ernews_app).
 -behaviour(application).
